659228b51c2d4a31bba3ceaa93a9505f8173d2ff
usuario <alu0101247519@ull.edu.es>
2021-11-18 23:51:20 +0000

    ej01_completo

--- p08_functions_github/ej01_three_words/three_words.cc
+++ p08_functions_github/ej01_three_words/three_words.cc
@@ -1,3 +1,17 @@
+/**
+  * Universidad de La Laguna
+  * Escuela Superior de Ingeniería y Tecnología
+  * Grado en Ingeniería Informática
+  * Informática Básica 2021-2022
+  *
+  * @file three_words.cc
+  * @author Guillermo Hernández Mesa alu0101247519@ull.edu.es
+  * @date Nov 18 2022
+  * @brief Write a program that reads three words a, b and c, and prints a line with c, b and a in this order.
+  * @bug There are no known bugs
+  * @see https://jutge.org/problems/P62421
+  */
+ 
 #include <iostream>
 
 int main() {
