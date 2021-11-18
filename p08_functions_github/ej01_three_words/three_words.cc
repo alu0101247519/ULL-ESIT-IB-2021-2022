e7bb1d4c9a966c7229668781542967ca77ecbf9b
usuario <alu0101247519@ull.edu.es>
2021-11-18 23:59:28 +0000

    ej01_completo

--- p08_functions_github/ej01_three_words/three_words.cc
+++ p08_functions_github/ej01_three_words/three_words.cc
@@ -18,4 +18,6 @@ int main() {
 	std::string palabra1, palabra2, palabra3;
 	std::cin >> palabra1 >> palabra2 >> palabra3;
 	std::cout << palabra3 << ' ' << palabra2 << ' ' << palabra1 << std::endl;
+
+	return 0;
 }
