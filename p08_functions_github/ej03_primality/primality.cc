d38fb26c5d6d82cb00390b03138d8103716a1ef1
usuario <alu0101247519@ull.edu.es>
2021-11-21 15:44:06 +0000

    ej03 + funciones

--- p08_functions_github/ej03_primality/primality.cc
+++ p08_functions_github/ej03_primality/primality.cc
@@ -7,37 +7,42 @@
   * @file primality.cc
   * @author Guillermo Hernández Mesa alu0101247519@ull.edu.es
   * @date Nov 18 2022
-  * @brief Write a program to print in order all the divisors of a given number.
+  * @brief Write a program that reads a sequence of natural numbers and, for each one, tells if it is a prime number or not. 
+  * Remember that a natural number is prime if and only if it is greater than 1 and 
+  * it does not have any positive divisor other than 1 and itself.
   * @bug There are no known bugs
-  * @see https://jutge.org/problems/P39308
+  * @see https://jutge.org/problems/P48713_en
   */
 
 #include <iostream>
 
+void resultado (int, int);
+
 int main() {
     
-    int numeros_introducidos;
-    std::cin >> numeros_introducidos;
+  int numeros_introducidos;
+  std::cin >> numeros_introducidos;
 
-    for(int i = 0; i < numeros_introducidos; ++i) {
-	int numero;
-        std::cin >> numero;
+  for(int i = 0; i < numeros_introducidos; ++i) {
+	  int numero;
+    std::cin >> numero;
 
-	bool primo = (numero != 1 && numero != 0);
-	int aux = 2;
-
-	
-	while (primo && aux*aux <= numero) {
+	  bool primo = (numero != 1 && numero != 0);
+	  int aux = 2;
+    
+	  while (primo && aux*aux <= numero) {
 	    primo = numero%aux != 0;
 	    ++aux;
-        }
-    
-        if (primo){
-            std::cout << numero << " is prime" << std::endl;
-        } else {
-            std::cout << numero << " is not prime" << std::endl;
-        }
-	    
     }
-    return 0;
+    resultado(primo, numero);
+  }
+  return 0;
+}
+
+void resultado (int primo, int numero) {
+  if (primo){
+    std::cout << numero << " is prime" << std::endl;
+  } else {
+    std::cout << numero << " is not prime" << std::endl;
+  }  
 }
